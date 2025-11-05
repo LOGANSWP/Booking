7b4f84b38abfab875ec0300812188a620ca0c6a6
LoganSu <logansu1025@outlook.com>
2025-11-04 23:23:35 -0600

    Complete sign in functionality

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,4 +1,8 @@
+import { useMutation } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import * as apiClient from "../api-client.js";
+import { useNavigate } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext.js";
 
 export type SignInFormData = {
   email: string;
@@ -6,13 +10,32 @@ export type SignInFormData = {
 };
 
 export default function SignIn() {
+  const navigate = useNavigate();
+  const { showToast } = useAppContext();
+
   const {
     register,
     formState: { errors },
+    handleSubmit,
   } = useForm<SignInFormData>();
 
+  const mutation = useMutation({
+    mutationFn: (formData: SignInFormData) => apiClient.signIn(formData),
+    onSuccess: async () => {
+      showToast({ message: "Sign in Success!", type: "SUCCESS" });
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
+
   return (
-    <form className="flex flex-col gap-5 ">
+    <form className="flex flex-col gap-5 " onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Sign In</h2>
 
       <label className="text-gray-700 text-sm font-bold flex-1">
@@ -43,6 +66,14 @@ export default function SignIn() {
           <span className="text-red-500">{errors.password.message}</span>
         )}
       </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Login
+        </button>
+      </span>
     </form>
   );
 }
