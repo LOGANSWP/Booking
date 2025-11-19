c39040c0a8cd0720e6a72b5e58da9518b72ffabf
LoganSu <logansu1025@outlook.com>
2025-11-18 23:02:20 -0600

    Setup Cloudinary config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,13 @@ import authRoutes from "./routes/auth.js";
 import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME as string,
+  api_key: process.env.CLOUDINARY_API_KEY as string,
+  api_secret: process.env.CLOUDINARY_API_SECRET as string,
+});
 
 if (!process.env.MONGODB_CONNECTION_STRING) {
   console.error("❌ Missing MONGODB_CONNECTION_STRING in .env");
