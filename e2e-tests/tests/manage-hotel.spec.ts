f120bde38c42c1ba9e82314e27be2469b04ede93
LoganSu <logansu1025@outlook.com>
2025-11-25 23:42:45 -0600

    Complete e2e test for add hotel

---
+++ e2e-tests/tests/manage-hotel.spec.ts
@@ -0,0 +1,55 @@
+import { test, expect } from "@playwright/test";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const UI_URL = "http://localhost:5173/";
+
+// Sign in before any tests
+test.beforeEach(async ({ page }) => {
+  await page.goto(UI_URL);
+
+  // Get the sign in button
+  await page.getByRole("link", { name: "Sign In" }).click();
+
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+  await page.locator("[name=email]").fill("test1@gmail.com");
+  await page.locator("[name=password]").fill("123456");
+
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Sign in Success!")).toBeVisible();
+});
+
+test("should allow user to add a hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}add-hotel`);
+
+  await page.locator('[name="name"]').fill("Test Hotel");
+  await page.locator('[name="city"]').fill("Test City");
+  await page.locator('[name="country"]').fill("Test Country");
+  await page
+    .locator('[name="description"]')
+    .fill("This is a description for the Test Hotel");
+  await page.locator('[name="pricePerNight"]').fill("100");
+  await page.selectOption('select[name="starRating"]', "3");
+
+  await page.getByText("Budget").click();
+
+  await page.getByLabel("Free Wifi").check();
+  await page.getByLabel("Parking").check();
+
+  await page.locator('[name="adultCount"]').fill("2");
+  await page.locator('[name="childCount"]').fill("4");
+
+  await page.setInputFiles('[name="imageFiles"]', [
+    path.join(__dirname, "files", "1.jpg"),
+    path.join(__dirname, "files", "2.jpg"),
+    path.join(__dirname, "files", "3.jpg"),
+  ]);
+
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Add Hotel Success!")).toBeVisible();
+});
