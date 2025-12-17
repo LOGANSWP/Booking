764a0708a100abaf6ca7f3a1484b3eef77880a1f
LoganSu <logansu1025@outlook.com>
2025-12-16 18:28:28 -0600

    Add StarRatingFilter to search page

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -4,10 +4,12 @@ import * as apiClient from "../api-client.js";
 import { useState } from "react";
 import SearchResultsCard from "../components/SearchResultsCard.js";
 import Pagination from "../components/Pagination.js";
+import StarRatingFilter from "../components/StarRatingFilter.js";
 
 export default function Search() {
   const search = useSearchContext();
   const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([]);
 
   const searchParams = {
     destination: search.destination,
@@ -16,12 +18,23 @@ export default function Search() {
     adultCount: search.adultCount.toString(),
     childCount: search.childCount.toString(),
     page: page.toString(),
+    stars: selectedStars,
   };
 
   const { data: hotelData } = useQuery({
     queryKey: ["searchHotels", searchParams],
     queryFn: () => apiClient.searchHotels(searchParams),
   });
+
+  const handleStarsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = event.target.value;
+
+    setSelectedStars((prevStars) =>
+      event.target.checked
+        ? [...prevStars, starRating]
+        : prevStars.filter((star) => star !== starRating)
+    );
+  };
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
@@ -30,6 +43,10 @@ export default function Search() {
           <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
             Filter by:
           </h3>
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
+          />
         </div>
       </div>
       <div className="flex flex-col gap-5">
